ba15a1f966ad3929b8fa828921deebea00429b92
Huy Nghiem <huynghiem2019@gmail.com>
2026-03-15 12:38:47 +0700

    Bump SW cache v17 to force update on deployed site

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js - Service Worker for offline support + SharedArrayBuffer headers
 // Injects COOP/COEP headers so Fairy-Stockfish WASM works on GitHub Pages
 
-const CACHE_NAME = 'co-tuong-v16';
+const CACHE_NAME = 'co-tuong-v17';
 const ASSETS = [
     './',
     './index.html',
