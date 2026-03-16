2b1beeaf7cdfaf5240484342fbfb4313db238bcb
Huy Nghiem <huynghiem2019@gmail.com>
2026-03-17 05:19:30 +0700

    Rename loser title to Poor Baby Boss + add profile images

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js - Service Worker for offline support + SharedArrayBuffer headers
 // Injects COOP/COEP headers so Fairy-Stockfish WASM works on GitHub Pages
 
-const CACHE_NAME = 'co-tuong-v23';
+const CACHE_NAME = 'co-tuong-v24';
 const ASSETS = [
     './',
     './index.html',
