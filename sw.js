ea140ee7c3c5baee1a4164e78eb2de29b7e5662d
Huy Nghiem <huynghiem2019@gmail.com>
2026-03-18 11:58:34 +0700

    Fix iPad: limit board to 55vh, column layout, controls always visible

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js - Service Worker for offline support + SharedArrayBuffer headers
 // Injects COOP/COEP headers so Fairy-Stockfish WASM works on GitHub Pages
 
-const CACHE_NAME = 'co-tuong-v30';
+const CACHE_NAME = 'co-tuong-v31';
 const ASSETS = [
     './',
     './index.html',
