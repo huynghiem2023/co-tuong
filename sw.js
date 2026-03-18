e27592eb8713a97dfbcb87c446dbde9852b85a8f
Huy Nghiem <huynghiem2019@gmail.com>
2026-03-18 12:05:05 +0700

    Show profile images on tablet: compact 60x75px equal size

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js - Service Worker for offline support + SharedArrayBuffer headers
 // Injects COOP/COEP headers so Fairy-Stockfish WASM works on GitHub Pages
 
-const CACHE_NAME = 'co-tuong-v31';
+const CACHE_NAME = 'co-tuong-v32';
 const ASSETS = [
     './',
     './index.html',
